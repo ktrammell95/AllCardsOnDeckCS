5f6e9d3feeed1a63eb9808dfb82b371bdf92440d
Katherine Trammell <ktrammell.com>
2021-01-08 18:58:37 -0500

    adding Gavin's example of using values and suits

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ namespace AllCardsOnDeckCS
         static void Main(string[] args)
         {
             var cards = new List<string>() { "Ace of Hearts", "Ace of Clubs", "Ace of Diamonds", "Ace of Spades", "2 of Hearts", "2 of Clubs", "2 of Diamonds", "2 of Spades", "3 of Hearts", "3 of Clubs", "3 of Diamonds", "3 of Spades", "4 of Hearts", "4 of Clubs", "4 of Diamonds", "4 of Spades", "5 of Hearts", "5 of Clubs", "5 of Diamonds", "5 of Spades", "6 of Hearts", "6 of Clubs", "6 of Diamonds", "6 of Spades", "7 of Hearts", "7 of Clubs", "7 of Diamonds", "7 of Spades", "8 of Hearts", "8 of Clubs", "8 of Diamonds", "8 of Spades", "9 of Hearts", "9 of Clubs", "9 of Diamonds", "9 of Spades", "10 of Hearts", "10 of Clubs", "10 of Diamonds", "10 of Spades", "Jack of Hearts", "Jack of Clubs", "Jack of Diamonds", "Jack of Spades", "Queen of Hearts", "Queen of Clubs", "Queen of Diamonds", "Queen of Spades", "King of Hearts", "King of Clubs", "King of Diamonds", "King of Spades" };
-            // var cardSuits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
-            // var cardValues = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 
             // numberOfCards = length of our deck
             var numberOfCards = cards.Count;
@@ -50,6 +48,27 @@ namespace AllCardsOnDeckCS
             var cardDealtOne = cards[0];
             var cardDealtTwo = cards[1];
             Console.WriteLine($"Cards dealt: {cardDealtOne} and {cardDealtTwo}");
+
+            Console.WriteLine($"------------------- Gavin's walkthrough -------------------");
+
+
+            var cardSuits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+            var cardValues = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            var newDeck = new List<string>();
+
+            foreach (var suit in cardSuits)
+            {
+                // Console.WriteLine($"{suit}");
+                foreach (var value in cardValues)
+                {
+                    // Console.WriteLine($"    {value}");
+
+                    var newString = $"{value} of {suit}";
+                    Console.WriteLine($"{newString}");
+
+                    newDeck.Add(newString);
+                }
+            }
         }
     }
 }
